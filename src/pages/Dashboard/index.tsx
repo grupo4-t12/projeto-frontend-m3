cb839aa89aa4c429106ede212ec797654d52dfb5
Axel <axeellima@gmail.com>
2022-09-02 12:22:40 -0300

    add: dashboard clinic component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,100 +1,19 @@
-import { useContext, useState } from "react";
-import { TiEdit } from "react-icons/ti";
-import { IoLogOutSharp, IoTrashOutline, IoPawSharp } from "react-icons/io5";
-
-import { UserContext } from "../../contexts/UserContext";
-import { PetContext } from "../../contexts/PetContext";
-
-import petImage from "../../assets/petImage.png";
-import EditPetModal from "../../components/EditPetModal";
-import DeletePetModal from "../../components/DeletePetModal";
-
-import { Container, LinkButton, Main } from "./styles";
 import { Navigate } from "react-router-dom";
+import DashboardClient from "../../components/DashboardClient";
+import DashboardClinic from "../../components/DashboardClinic";
 
 function Dashboard() {
-  const { setUser, listPets } = useContext(UserContext);
-  const { editModal, setEditModal, deleteModal, setDeleteModal, setPetId } =
-    useContext(PetContext);
-
-  const [name, setName] = useState("");
-  const [animal, setAnimal] = useState("");
-
   const token = localStorage.getItem("@TOKEN");
-
-  function handleClick() {
-    localStorage.clear();
-    setUser(null);
-  }
+  const idUser = localStorage.getItem("@USERID");
 
   return token ? (
-    <Container>
-      <header>
-        <h1>
-          SEU PET <IoPawSharp />
-        </h1>
-        <LinkButton onClick={handleClick} to="/">
-          <IoLogOutSharp size={30} />
-        </LinkButton>
-      </header>
-      <Main>
-        <div className="menu">
-          <button>Meus pets</button>
-          <button>Consultas</button>
-          <button>Vacinas</button>
-          <button
-            onClick={() => {
-              window.open("https://calendly.com/axeellima/seupet-consulta");
-            }}
-          >
-            Agendamentos
-          </button>
-        </div>
-        <div className="pet-box">
-          <h3>Meus Pets:</h3>
-          <div className="pet-info">
-            <p className="list-pet">Pet:</p>
-            <p className="list-animal">Animal:</p>
-            <p className="list-edit">Editar:</p>
-          </div>
-          <div className="pet-data">
-            {listPets.map((pet) => (
-              <div className="data" key={pet.id}>
-                <p className="list-pet">{pet.name}</p>
-                <p className="list-animal">{pet.animal}</p>
-                <div className="buttons list-edit">
-                  <TiEdit
-                    onClick={() => {
-                      setPetId(pet.id);
-                      setName(pet.name);
-                      setAnimal(pet.animal);
-                      setEditModal(true);
-                    }}
-                    size={28}
-                  />
-                  <IoTrashOutline
-                    onClick={() => {
-                      setPetId(pet.id);
-                      setDeleteModal(true);
-                    }}
-                    size={23}
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <aside>
-          <p>Acompanhe seu pet com a gente, Amar é cuidar!</p>
-          <img src={petImage} alt="" />
-          <span>Agende uma consulta</span>
-        </aside>
-      </Main>
-      {editModal && <EditPetModal name={name} animal={animal} />}
-      {deleteModal && <DeletePetModal />}
-    </Container>
+    idUser === "9" ? (
+      <DashboardClinic />
+    ) : (
+      <DashboardClient />
+    )
   ) : (
-    <Navigate to={"/login"} />
+    <Navigate to="/login" />
   );
 }
 
